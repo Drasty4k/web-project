7a2fdc34c0e6739c0f4cb0ecf501076ca1f30553
Dragos Barbuta <barbuta.dragos12@gmail.com>
2022-09-25 12:36:28 +0300

    implemented the static method builUser on class User that will be called with some starting initial attributes for a user

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,4 +1,7 @@
 import { Model } from "./Model";
+import { Attributes } from "./Attributes";
+import { ApiSync } from "./ApiSync";
+import { Eventing } from "./Eventing";
 
 export interface UserProps {
   id?: number;
@@ -6,4 +9,14 @@ export interface UserProps {
   age?: number;
 }
 
-export class User extends Model<UserProps> {}
+const rootUrl = "http://localhost:3000/users";
+
+export class User extends Model<UserProps> {
+  static buildUser(attrs: UserProps): User {
+    return new User(
+      new Attributes<UserProps>(attrs),
+      new Eventing(),
+      new ApiSync<UserProps>(rootUrl)
+    );
+  }
+}
