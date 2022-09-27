375c8693054aeebf67183453d0427addd891e1f4
Dragos Barbuta <barbuta.dragos12@gmail.com>
2022-09-27 20:10:51 +0300

    Implemented the onRender method, which is called before the template is appended to the parent, allowing the creation of Nested templates. Implemented the onRender method, which is called before the template is appended to the parent, allowing the creation of Nested templates. And fixed a typo in how the selector is chosen from the fragment.

--- src/views/View.ts
+++ src/views/View.ts
@@ -1,7 +1,7 @@
 import { HasId, Model } from "../models/Model";
 
 export abstract class View<T extends Model<K>, K extends HasId> {
-  regions: { [key: string]: NodeListOf<Element> } = {};
+  regions: { [key: string]: Element } = {};
 
   constructor(public parent: Element, public model: T) {
     this.bindModel();
@@ -39,13 +39,15 @@ export abstract class View<T extends Model<K>, K extends HasId> {
 
     for (let key in regionsMap) {
       const selector = regionsMap[key];
-      const element = fragment.querySelectorAll(selector);
+      const element = fragment.querySelector(selector);
 
       if (element) {
         this.regions[key] = element;
       }
     }
   }
+
+  onRender(): void {}
 
   render(): void {
     this.parent.innerHTML = "";
@@ -55,6 +57,8 @@ export abstract class View<T extends Model<K>, K extends HasId> {
 
     this.bindEvents(templateElement.content);
     this.mapRegions(templateElement.content);
+
+    this.onRender();
 
     this.parent.append(templateElement.content);
   }
