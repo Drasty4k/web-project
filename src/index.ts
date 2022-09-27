5a2f04f0b548aad683fa0cff76bcecb2aaa7fef3
Dragos Barbuta <barbuta.dragos12@gmail.com>
2022-09-27 14:33:17 +0300

    added Model Properties to the UserForm class and render the model info in the html template

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 import { UserForm } from "./views/UserForm";
+import { User } from "./models/User";
 
-const useForm = new UserForm(document.getElementById("root")!);
+const user = User.buildUser({ name: "NAME", age: 20 })
+
+const useForm = new UserForm(document.getElementById("root")!, user);
 
 useForm.render()
